8e1b46acc23424662ea7b4a54c162799f396b8af
nicolearantes <nicolearantes@users.noreply.github.com>
2020-10-09 10:21:14 +0200

    Adding CPU action

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 $(".player1-button").on("click", function (e) {
    var item =  $(this).data("which-button");
-    console.log(item)
+    var action = ["rock", "paper", "scissors"]
+    var select = Math.floor(Math.random() * 3);
+    var random = action[select]; 
+
     $("#player1-choose")
         .removeClass()
         .addClass("fas")
         .addClass("fa-hand-" + item)
         .addClass("fa-10x")
 
+    $("#player2-choose")
+        .removeClass()
+        .addClass("fas")
+        .addClass("fa-hand-" + random)
+        .addClass("fa-10x")
     
 });
+
+
+
+
+ 
+
 
 
 
